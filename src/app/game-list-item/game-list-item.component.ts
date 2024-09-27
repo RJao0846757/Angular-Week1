aefd84be955189171a1fc1aa92ea70b5db4a007c
Ralph-Julien Jao <W0846757@myscc.ca>
2024-09-26 23:54:39 -0400

    passes each item from game list into an item tag in list component html
    - GameArray and its objects are now strings for simplification
    - Input in list item now takes any data type

--- src/app/game-list-item/game-list-item.component.ts
+++ src/app/game-list-item/game-list-item.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './game-list-item.component.scss'
 })
 export class GameListItemComponent {
-  @Input() gameTitle?: String;
+  @Input() gameData?: any;
 }
--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { Game } from '../models/game';
 import { GameListItemComponent } from "../game-list-item/game-list-item.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [GameListItemComponent],
+  imports: [GameListItemComponent, CommonModule],
   templateUrl: './game-list.component.html',
   styleUrl: './game-list.component.scss'
 })
 export class GameListComponent {
-  gameListObj1 : Game = {name: "Among Us", price: 4.99, releaseDate: "06/15/18", developer: "Innersloth"};
-  gameListObj2 : Game = {name: "Red Dead Redemption 2", price: 79.99, releaseDate: "10/26/18", developer: "Rockstar Games"};
-  gameListObj3 : Game = {name: "Ace Combat 7: Skies Unknown", price: 59.99, releaseDate: "01/18/19", developer: "Bandai Namco Studios", publisher: "Bandai Namco Entertainment"};
-  gameListObj4 : Game = {name : "Not A Real Game", price : 0.00, releaseDate : "01/01/00", developer : "Imaginary Developers"};
+  gameListObj1 : String = "Among us";
+  gameListObj2 : String = "Red Dead Redemption 2";
+  gameListObj3 : String = "Ace Combat 7: Skies Unknown";
+  gameListObj4 : String = "Not A Real Game";
 
-  gameArray : Game[] = [this.gameListObj1, this.gameListObj2, this.gameListObj3, this.gameListObj4];
+  gameArray : String[] = [this.gameListObj1, this.gameListObj2, this.gameListObj3, this.gameListObj4];
 }
