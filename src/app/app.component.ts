67e0ce3932a4a6d191e0166531b76a8e034acf79
Ralph-Julien Jao <W0846757@myscc.ca>
2024-09-26 23:14:00 -0400

    Creates several tags in html component, defines array in ts component
    - game list tag is in component.html
    - component.ts contains an array of 4 games
    - gamelistitem.ts has a property that accepts an input to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Game } from './models/game';
 import {NgForOf, NgIf} from "@angular/common";
+import { GameListComponent } from "./game-list/game-list.component";
+import { GameListItemComponent } from "./game-list-item/game-list-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgIf],
+  imports: [RouterOutlet, NgForOf, NgIf, GameListComponent, GameListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,12 +16,12 @@ export class AppComponent {
   title = 'Assignment 1';
   name = 'Ralph-Julien Jao';
 
-  game1 : Game = {name : "TestGame", price : 59.99, releaseDate : "09/19/24", developer : "SoloDev"}
-  game2 : Game = {name : "BlockGame", price : 39.99, releaseDate : "02/18/18", developer : "BigTeam", publisher: "BigCo"}
-  game3 : Game = {name : "Skyrim", price : 59.99, releaseDate : "11/11/11", developer : "Bethesda"}
-  game4 : Game = {name : "FortGame", price : 0.00, releaseDate : "07/25/17", developer : "Legendary Studios", publisher: "Legendary Games"}
-  game5 : Game = {name : "GameGame", price : 79.99, releaseDate : "12/15/19", developer : "Game Studios", publisher : "Game Publishing"}
-  game6 : Game = {name : "Star Game 2 (2005)", price : 29.99, releaseDate : "10/28/05", developer : "Epidemic Studios", publisher : "LukeArts"}
+  game1 : Game = {name : "TestGame", price : 59.99, releaseDate : "09/19/24", developer : "SoloDev"};
+  game2 : Game = {name : "BlockGame", price : 39.99, releaseDate : "02/18/18", developer : "BigTeam", publisher: "BigCo"};
+  game3 : Game = {name : "Skyrim", price : 59.99, releaseDate : "11/11/11", developer : "Bethesda"};
+  game4 : Game = {name : "FortGame", price : 0.00, releaseDate : "07/25/17", developer : "Legendary Studios", publisher: "Legendary Games"};
+  game5 : Game = {name : "GameGame", price : 79.99, releaseDate : "12/15/19", developer : "Game Studios", publisher : "Game Publishing"};
+  game6 : Game = {name : "Star Game 2 (2005)", price : 29.99, releaseDate : "10/28/05", developer : "Epidemic Studios", publisher : "LukeArts"};
 
   gameList : Game[] = [this.game1, this.game2, this.game3, this.game4, this.game5, this.game6];
 }
