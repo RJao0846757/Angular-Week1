1e395eeb478fac87cf9de8d848206fb9f1f04b74
Ralph-Julien Jao <W0846757@myscc.ca>
2024-10-01 10:34:41 -0400

    moves IContent array into new file mock-content.ts, creates service

---
+++ src/app/game-list/data/mock-content.ts
@@ -0,0 +1,9 @@
+export class GameListComponent {
+    gameListObj1 : String = "Among us";
+    gameListObj2 : String = "Red Dead Redemption 2";
+    gameListObj3 : String = "Ace Combat 7: Skies Unknown";
+    gameListObj4 : String = "Not A Real Game";
+  
+    gameArray : String[] = [this.gameListObj1, this.gameListObj2, this.gameListObj3, this.gameListObj4];
+  }
+  
--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Game } from '../models/game';
 import { GameListItemComponent } from "../game-list-item/game-list-item.component";
 import { CommonModule } from '@angular/common';
 
@@ -11,10 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './game-list.component.scss'
 })
 export class GameListComponent {
-  gameListObj1 : String = "Among us";
-  gameListObj2 : String = "Red Dead Redemption 2";
-  gameListObj3 : String = "Ace Combat 7: Skies Unknown";
-  gameListObj4 : String = "Not A Real Game";
 
-  gameArray : String[] = [this.gameListObj1, this.gameListObj2, this.gameListObj3, this.gameListObj4];
 }
