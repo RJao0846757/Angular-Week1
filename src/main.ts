eb56463c13a505a39cbd9dd2af97e84f7c00fbfd
Ralph-Julien Jao <W0846757@myscc.ca>
2024-10-11 11:02:50 -0400

    Sets up router in main.ts
    - defines game list routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { GameListComponent } from './app/game-list/game-list.component';
+import { GameListItemComponent } from './app/game-list-item/game-list-item.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: 'app/game-list/', component: GameListComponent},
+  {path: 'app/game-list/', component: GameListItemComponent}
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
