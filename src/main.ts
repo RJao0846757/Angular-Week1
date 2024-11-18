74207135dd9cd55734ed708a5bf2dc83bc2b4202
Ralph-Julien Jao <W0846757@myscc.ca>
2024-11-17 21:11:30 -0500

    implements lazy loading into page

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
 import { GameListComponent } from './app/game-list/game-list.component';
-import { GameListItemComponent } from './app/game-list-item/game-list-item.component';
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
@@ -13,9 +10,12 @@ bootstrapApplication(AppComponent, appConfig)
 const routes: Routes = [
   {path: '', redirectTo:"/game-list", pathMatch: 'full'},
   {path: 'game-list', component: GameListComponent},
-  {path: 'game-list-item', component: GameListItemComponent},
-  {path: 'modify-list-item', component: ModifyListItemComponent},
-  {path: '**', component: PageNotFoundComponent} // 404 route
+  {path: 'game-list-item', loadComponent: () =>
+      import('./app/game-list-item/game-list-item.component').then(m => m.GameListItemComponent)},
+  {path: 'modify-list-item', loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)},
+  {path: '**', loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then((m => m.PageNotFoundComponent))} // 404 route
 ]
 
 bootstrapApplication(AppComponent, {
